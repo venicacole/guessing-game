d2c993c52c5fff9b65ff3050dec395aa09dac969
venicacole <venicacole@gmail.com>
2024-09-18 19:58:00 -0400

    This is the Prompt for Player's Name.

--- guessinggame.js
+++ guessinggame.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+// console.log("hello world")
+
+// 1. Research the `Math.random()` method to learn how to generate a random number. The number should be between 1-50.
+// 2. Give a user a prompt to guess the random number. NOTE: You will need to keep track of the number of guesses a user has used.
+// 3. Use if/else and truthy/falsy concepts to evaluate whether or not the user's guess was correct.
+// 4. NOTE: If a user's guess is incorrect, respond with a prompt letting them know whether or not their guess was too high or too low and how many chances remain.
+// 5. If a user guesses within three chances, display an alert letting the user know they've won and what the number was.
+// 6. If a user loses, display an alert letting the user know they have lost and reveal the number.
+
+//Prompt for Name
+let playerName = prompt("Hey! What's Your Name?");
+console.log("The player's name", playerName);
+
+
+
+
+
+
+//const randomNumber = Math.floor(Math.random() * 50 + 1;
